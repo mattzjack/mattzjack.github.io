f2688c7618dd742fafc8010f094e53c6885b0e19
mattzjack <MK@Okceg.local>
2015-04-13 22:26:10 +0800

    learn latin + latin name
    finished day 1 and remade latin name

--- latin_name/script.js
+++ latin_name/script.js
@@ -1,4 +1,4 @@
 function msg() {
   msg = document.getElementById("msg");
-  msg.innerHTML = "Check my <a href=\"latin_name_generator.py\">python file</a>!\nRun it yourself, lazy.";
+  msg.innerHTML = "Check my <a href=\"latin_name_generator.py\">python file</a>!<br>Run it yourself, lazy.<br>p.s. get <a href=\"https://www.python.org/\">python 3</a>.";
 }
