122701e6bb5c46afc76c3ec9708ba21d6e76ff6d
Vibius Vibidius Zosimus <mudsharkmk@gmail.com>
2016-12-03 02:14:29 -0500

    damn I forgot to eat

--- learn_latin/day_2/script.js
+++ learn_latin/day_2/script.js
@@ -1,26 +1,15 @@
-alert("Hello, I am Vibius Vibidius Zosimus.");
-alert("pls don't check this box below because I am an innocuous script...");
-var a;
-var b = true;
-while(b)
-{
-  a = prompt("enter your Latin full name:\nif you have forgotten, enter EXACTLY \"Igpay Atinlay\".");
-  if (a == "Igpay Atinlay")
-  {
-    alert("remember your name!\nand Igpay Atinlay is one thing that you are not going to learn about here..");
-    b = false;
-  }
-  else if (a == null | a == "")
-  {
-    alert("at least enter something here...");
-  }
-  else
-  {
-    alert("welcome back, " + a + "... what a name...");
-    b = false;
-  }
-}
-if (!b)
-{
-  document.getElementById("hidden").innerHTML = "<h2>prologue</h2><h6>the magic of javascript</h6><p>Hopefully you didn't keep refreshing and flirt with my script a lot...</p>";
-}
+$(function(){
+    alert("Hello, I am Vibius Vibidius Zosimus.");
+    alert("pls don't check this box below because I am an innocuous script...");
+    var a = prompt("enter your Latin full name:\nif you have forgotten, enter EXACTLY \"igpay atinlay\".");
+    if (a == "igpay atinlay") {
+      alert("remember your name!\nand pig latin is one thing that you are not going to learn about here..");
+      $("#hidden").html("<h2>prologue</h2><h6>the magic of javascript</h6><p>Hopefully you didn't keep refreshing and flirt with my script a lot...</p>");
+    } else if (a == null | a == "") {
+      alert("at least enter something there.. whatever..");
+      $("#hidden").html("<h2>prologue</h2><h6>the magic of javascript</h6><p>Hopefully you didn't keep refreshing and flirt with my script a lot...</p>");
+    } else {
+      alert("welcome back, " + a + ".. what a name..");
+      $("#hidden").html("<h2>prologue</h2><h6>the magic of javascript</h6><p>Hopefully you didn't keep refreshing and flirt with my script a lot...</p>");
+    }
+});
