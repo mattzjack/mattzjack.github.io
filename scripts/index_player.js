6bb384ff303c05e1e44f5a12d58c569c7523e333
Vibius Vibidius Zosimus <mudsharkmk@gmail.com>
2017-12-09 18:40:56 -0500

    more un-ddebug

--- scripts/index_player.js
+++ scripts/index_player.js
@@ -2,7 +2,6 @@ $(function() {
     var iframeElement   = document.querySelector('iframe');
     var iframeElementID = iframeElement.id;
     var widget          = SC.Widget(iframeElement);
-    var x = 1;
 
     $("#togglePlay").click(function() {
         widget.toggle();
