33e47cb6f9b05571237f8f76b9103b9c219de337
Vibius Vibidius Zosimus <mudsharkmk@gmail.com>
2018-03-25 18:36:39 -0400

    new song yey

--- scripts/index_player.js
+++ scripts/index_player.js
@@ -1,9 +1,16 @@
 $(function() {
-    var iframeElement   = document.querySelector('iframe');
-    var iframeElementID = iframeElement.id;
-    var widget          = SC.Widget(iframeElement);
+    var iframeElement    = document.querySelector('#song1');
+    var iframeElement2   = document.querySelector('#song2');
+    var iframeElementID  = iframeElement.id;
+    var iframeElementID2 = iframeElement2.id;
+    var widget           = SC.Widget(iframeElement);
+    var widget2          = SC.Widget(iframeElement2);
 
     $("#togglePlay").click(function() {
         widget.toggle();
+    });
+
+    $("#togglePlay2").click(function() {
+        widget2.toggle();
     });
 });
